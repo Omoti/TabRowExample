6b698a0858c190ecbede9be0742c23528c045b28
Omoti <guti.tomtom@gmail.com>
2023-02-24 06:35:50 +0900

    feat: ScrollableTabRow

--- app/src/main/java/com/omoti/tabsexample/screens/ScrollableTabRowScreen.kt
+++ app/src/main/java/com/omoti/tabsexample/screens/ScrollableTabRowScreen.kt
@@ -1,8 +1,80 @@
 package com.omoti.tabsexample.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.omoti.tabsexample.ui.theme.TabsExampleTheme
 
+/**
+ * Scrollable Tab Sample
+ * https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#ScrollableTabRow(kotlin.Int,androidx.compose.ui.Modifier,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.unit.Dp,kotlin.Function1,kotlin.Function0,kotlin.Function0)
+ */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScrollableTabRowScreen(onBack: () -> Unit) {
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val titles = listOf("Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5", "Don’t truncate")
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Scrollable TabRow") },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            ScrollableTabRow(selectedTabIndex = selectedTabIndex) {
+                titles.forEachIndexed { index, title ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = { selectedTabIndex = index },
+                        text = { Text(text = title, maxLines = 1) },
+                    )
+                }
+            }
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(
+                    text = "Text tab ${selectedTabIndex + 1} selected",
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ScrollableTabRowScreenPreview() {
+    TabsExampleTheme {
+        ScrollableTabRowScreen(onBack = {})
+    }
 }
