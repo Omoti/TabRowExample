0c0d2f31ab37f63bc1073107c120fc2a2e1fe645
Omoti <guti.tomtom@gmail.com>
2023-02-26 17:24:05 +0900

    feat: TabRow in Sticky Header

--- app/src/main/java/com/omoti/tabsexample/screens/StickyScreen.kt
+++ app/src/main/java/com/omoti/tabsexample/screens/StickyScreen.kt
@@ -1,7 +1,12 @@
 package com.omoti.tabsexample.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -20,14 +25,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.omoti.tabsexample.ui.theme.TabsExampleTheme
 
 /**
  * FixedTabRow in Sticky Header
  */
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun StickyScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
     var selectedTabIndex by remember { mutableStateOf(initialTabIndex) }
@@ -50,16 +58,29 @@ fun StickyScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
                 .fillMaxSize()
                 .padding(paddingValues),
         ) {
-            TabRow(selectedTabIndex = selectedTabIndex) {
-                titles.forEachIndexed { index, title ->
-                    Tab(
-                        selected = selectedTabIndex == index,
-                        onClick = { selectedTabIndex = index },
-                        text = { Text(text = title, maxLines = 1) },
-                    )
-                }
-            }
             LazyColumn(modifier = Modifier.fillMaxSize()) {
+                item {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .height(200.dp)
+                            .fillMaxWidth()
+                            .background(color = Color.LightGray),
+                    ) {
+                        Text(text = "Top Content")
+                    }
+                }
+                stickyHeader {
+                    TabRow(selectedTabIndex = selectedTabIndex) {
+                        titles.forEachIndexed { index, title ->
+                            Tab(
+                                selected = selectedTabIndex == index,
+                                onClick = { selectedTabIndex = index },
+                                text = { Text(text = title, maxLines = 1) },
+                            )
+                        }
+                    }
+                }
                 items(30) { index ->
                     ListItem(headlineText = { Text(text = "item $index") })
                 }
