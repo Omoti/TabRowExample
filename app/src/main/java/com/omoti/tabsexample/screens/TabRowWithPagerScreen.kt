67c3af90c78cf5fea2827b59bb2d71da4643d82f
Omoti <guti.tomtom@gmail.com>
2023-03-04 09:20:24 +0900

    feat: androidx HorizontalPager

--- app/src/main/java/com/omoti/tabsexample/screens/TabRowWithPagerScreen.kt
+++ app/src/main/java/com/omoti/tabsexample/screens/TabRowWithPagerScreen.kt
@@ -1,14 +1,17 @@
 package com.omoti.tabsexample.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -20,17 +23,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
 import com.omoti.tabsexample.ui.theme.TabsExampleTheme
 import kotlinx.coroutines.launch
 
 /**
  * TabRow with HorizontalPager
- * https://google.github.io/accompanist/pager/
  */
-@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun TabRowWithPagerScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
     val pagerState = rememberPagerState(initialPage = initialTabIndex)
@@ -40,7 +39,7 @@ fun TabRowWithPagerScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Fixed TabRow") },
+                title = { Text(text = "TabRow with HorizontalPager") },
                 navigationIcon = {
                     IconButton(onClick = onBack) {
                         Icon(Icons.Default.ArrowBack, contentDescription = null)
@@ -69,7 +68,7 @@ fun TabRowWithPagerScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
             }
             HorizontalPager(
                 state = pagerState,
-                count = titles.size,
+                pageCount = titles.size,
             ) { page ->
                 Box(
                     contentAlignment = Alignment.Center,
