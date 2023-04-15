27c5d3b54b2da312f84dfacbbb06a16383d34a30
omoti <guti.tomtom@gmail.com>
2023-04-15 18:17:53 +0900

    feat: use HorizontalPager in CollapseScreen

--- app/src/main/java/com/omoti/tabsexample/screens/CollapseScreen.kt
+++ app/src/main/java/com/omoti/tabsexample/screens/CollapseScreen.kt
@@ -1,9 +1,12 @@
 package com.omoti.tabsexample.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,22 +21,21 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import com.omoti.tabsexample.ui.theme.TabsExampleTheme
+import kotlinx.coroutines.launch
 
 /**
  * Collapse TopAppBar with FixedTabRow
  */
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun CollapseScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
-    var selectedTabIndex by remember { mutableStateOf(initialTabIndex) }
+    val pagerState = rememberPagerState(initialPage = initialTabIndex)
+    val coroutineScope = rememberCoroutineScope()
     val titles = listOf("Tab 1", "Tab 2", "Tab 3")
 
     val topAppBarState = rememberTopAppBarState()
@@ -58,18 +60,24 @@ fun CollapseScreen(onBack: () -> Unit, initialTabIndex: Int = 0) {
                 .fillMaxSize()
                 .padding(paddingValues),
         ) {
-            TabRow(selectedTabIndex = selectedTabIndex) {
+            TabRow(selectedTabIndex = pagerState.currentPage) {
                 titles.forEachIndexed { index, title ->
                     Tab(
-                        selected = selectedTabIndex == index,
-                        onClick = { selectedTabIndex = index },
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        },
                         text = { Text(text = title, maxLines = 1) },
                     )
                 }
             }
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(30) { index ->
-                    ListItem(headlineText = { Text(text = "item $index") })
+            HorizontalPager(pageCount = titles.size, state = pagerState) { page ->
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(30) { index ->
+                        ListItem(headlineText = { Text(text = "item $index") })
+                    }
                 }
             }
         }
